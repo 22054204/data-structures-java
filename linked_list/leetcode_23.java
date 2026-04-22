a5cd97739848b7299fa1c874a9c0ba2a023b68e0
RANJAN SHARMA <22054204@kiit.ac.in>
2026-04-22 22:54:16 +0530

    initial commit

---
+++ linked_list/leetcode_23.java
@@ -0,0 +1,73 @@
+//leetcode-23
+//Merge K sorted Lists
+//https://leetcode.com/problems/merge-k-sorted-lists/description/
+package linked_list;
+import java.util.*;
+public class leetcode_23 {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public static void main(String[] args) {
+        leetcode_23 obj = new leetcode_23();
+
+        // Create sample lists:
+        // [1->4->5], [1->3->4], [2->6]
+
+        ListNode l1 = new ListNode(1, new ListNode(4, new ListNode(5)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+        ListNode l3 = new ListNode(2, new ListNode(6));
+
+        ListNode[] lists = {l1, l2, l3};
+
+        ListNode result = obj.mergeKLists(lists);
+
+        printList(result);
+    }
+    // helper function
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        ArrayList<Integer> nums = new ArrayList<>();
+        int i = 0;
+        while(i<lists.length){
+            ListNode curr = lists[i];
+            while(curr!=null){
+                nums.add(curr.val);
+                curr = curr.next;
+            }
+            i++;
+        }
+        if(nums.size() == 0) return null;
+
+        Collections.sort(nums);
+
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+
+        i = 0;
+        while(i<nums.size()){
+            temp.next = new ListNode(nums.get(i));
+            i++;
+            temp = temp.next;
+        }
+        return dummy.next;
+    }
+}
---
+++ linked_list/leetcode_707.java
@@ -0,0 +1,134 @@
+package linked_list;
+
+public class leetcode_707 {
+    public static void main(String[] args) {
+        MyLinkedList list = new MyLinkedList();
+
+        list.addAtHead(10);
+        list.addAtHead(20);
+        list.addAtTail(30);
+        list.addAtIndex(1, 15);   // insert at index 1
+
+        System.out.println("Value at index 0: " + list.get(0));
+        System.out.println("Value at index 1: " + list.get(1));
+        System.out.println("Value at index 2: " + list.get(2));
+        System.out.println("Value at index 3: " + list.get(3));
+
+        list.deleteAtIndex(2);    // delete element at index 2
+
+        System.out.println("\nAfter deletion:");
+        printList(list);
+    }
+    // helper function to print list
+    public static void printList(MyLinkedList list) {
+        MyLinkedList.Node temp = list.head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    static class MyLinkedList {
+
+        static class Node{
+            int data;
+            Node next;
+
+            Node(int data){
+                this.data = data;
+                this.next = null;
+            }
+        }
+
+        Node head;
+
+        public MyLinkedList() {
+            head = null;
+        }
+
+        public int get(int index) {
+
+            Node temp = head;
+            int i = 0;
+
+            while(temp != null){
+                if(i == index){
+                    return temp.data;
+                }
+                temp = temp.next;
+                i++;
+            }
+            return -1;
+        }
+
+        public void addAtHead(int val) {
+
+            Node newNode = new Node(val);
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public void addAtTail(int val) {
+
+            Node newNode = new Node(val);
+
+            if(head == null){
+                head = newNode;
+                return;
+            }
+
+            Node temp = head;
+
+            while(temp.next != null){
+                temp = temp.next;
+            }
+
+            temp.next = newNode;
+        }
+
+        public void addAtIndex(int index, int val) {
+
+            if(index == 0){
+                addAtHead(val);
+                return;
+            }
+
+            Node temp = head;
+            int i = 0;
+
+            while(temp != null && i < index-1){
+                temp = temp.next;
+                i++;
+            }
+
+            if(temp == null) return;
+
+            Node newNode = new Node(val);
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+
+        public void deleteAtIndex(int index) {
+
+            if(head == null) return;
+
+            if(index == 0){
+                head = head.next;
+                return;
+            }
+
+            Node temp = head;
+            int i = 0;
+
+            while(temp.next != null && i < index-1){
+                temp = temp.next;
+                i++;
+            }
+
+            if(temp.next == null) return;
+
+            temp.next = temp.next.next;
+        }
+    }
+}
