f094c5b6c78ed83df88114a7f1dc28c1ec6269d4
RANJAN SHARMA <22054204@kiit.ac.in>
2026-04-22 22:58:29 +0530

    initial commit

---
+++ arrays/leetcode_189.java
@@ -0,0 +1,29 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class leetcode_189 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 3;
+        rotate(nums, k);
+        System.out.println(Arrays.toString(nums));
+    }
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k%n;
+        swap(nums, 0, n-1);
+        swap(nums, 0, k-1);
+        swap(nums, k, n-1);
+
+    }
+    public static void swap(int[] nums, int i, int j){
+        while(i<j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
