fe4714d2474cf30e884397d4e008cc3b63bd1902
RANJAN SHARMA <22054204@kiit.ac.in>
2026-04-22 23:06:39 +0530

    initial commit

---
+++ Strings/leetcode_1980.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class leetcode_1980 {
+    public static void main(String[] args) {
+        String[] nums = {"01","10"};
+        String s = findDifferentBinaryString(nums);
+        System.out.println(s);
+    }
+    public static String findDifferentBinaryString(String[] nums) {
+        int n = nums.length;
+        char[] result = new char[n];
+
+        for(int i = 0; i < n; i++){
+            if(nums[i].charAt(i) == '0'){
+                result[i] = '1';
+            } else {
+                result[i] = '0';
+            }
+        }
+
+        return new String(result);
+    }
+}
