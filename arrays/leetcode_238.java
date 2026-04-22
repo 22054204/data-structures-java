98df18db2e5a4d69e3f44b5ea4fc111421a98fd1
RANJAN SHARMA <22054204@kiit.ac.in>
2026-04-22 23:03:54 +0530

    initial commit

---
+++ arrays/leetcode_238.java
@@ -0,0 +1,25 @@
+package arrays;
+import java.util.*;
+public class leetcode_238 {
+    public static void main(String[] args) {
+        int[] nums = {-1,1,0,-3,3};
+        int[] result = productExceptSelf(nums);
+        System.out.println(Arrays.toString(result));
+    }
+    public static int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];//[1,]
+
+        result[0] = 1;
+        for(int i=1 ;i<nums.length;i++){
+            result[i] = nums[i-1]*result[i-1];
+        }
+
+        int suffix = 1;
+        for(int i=nums.length-1 ;i>=0;i--){
+            result[i] = result[i]*suffix;
+            suffix = suffix * nums[i];
+        }
+
+        return result;
+    }
+}
